0e2b0835ba890cf1a9ed7ee5a1b4f2514463bfaa
Accord99 <114819430+Accord99@users.noreply.github.com>
2023-08-18 11:37:11 +0300

    Русифицировали таблицу

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50,
+                            verbose_name='Название населённого пункта')
     slag = models.CharField(max_length=50,blank=True)
+    class Meta:
+        verbose_name = 'Название населённого пункта'
+        verbose_name_plural = 'Название населённых пунктов'
+    def __str__(self):
+        return self.name
 
