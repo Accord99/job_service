ff080189af41950ce0cac8ecc440423646c4c0c8
Accord99 <114819430+Accord99@users.noreply.github.com>
2023-08-18 13:24:30 +0300

    Создали ещё одну таблицу(класс) - Язык программирования, добавили файл utils.py - словарь конвертации русск букв в агл для слага(slug)

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -3,11 +3,27 @@ from django.db import models
 # Create your models here.
 class City(models.Model):
     name = models.CharField(max_length=50,
-                            verbose_name='Название населённого пункта')
-    slag = models.CharField(max_length=50,blank=True)
+                            verbose_name='Название населённого пункта',
+                            unique=True)
+    slag = models.CharField(max_length=50,
+                            blank=True,
+                            unique=True)
     class Meta:
         verbose_name = 'Название населённого пункта'
         verbose_name_plural = 'Название населённых пунктов'
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирования',
+                            unique=True)
+    slag = models.CharField(max_length=50,
+                            blank=True,
+                            unique=True)
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
     def __str__(self):
         return self.name
 
