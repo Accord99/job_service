822c1523176d91fd14ae827f9ed10f5b97372d04
Accord99 <114819430+Accord99@users.noreply.github.com>
2023-08-18 14:53:29 +0300

    Дописали код в models.py чтобы слаги заполнялись автоматически Функция называется save

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from scraping.utils import from_cyrillic_to_eng
+
+
 # Create your models here.
 class City(models.Model):
     name = models.CharField(max_length=50,
@@ -8,11 +11,21 @@ class City(models.Model):
     slag = models.CharField(max_length=50,
                             blank=True,
                             unique=True)
+
     class Meta:
         verbose_name = 'Название населённого пункта'
         verbose_name_plural = 'Название населённых пунктов'
+
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slag:
+            self.slag = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
+
+
 
 class Language(models.Model):
     name = models.CharField(max_length=50,
@@ -21,9 +34,16 @@ class Language(models.Model):
     slag = models.CharField(max_length=50,
                             blank=True,
                             unique=True)
+
     class Meta:
         verbose_name = 'Язык программирования'
         verbose_name_plural = 'Языки программирования'
+
     def __str__(self):
         return self.name
 
+
+    def save(self, *args, **kwargs):
+        if not self.slag:
+            self.slag = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
