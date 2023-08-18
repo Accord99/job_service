b9c13574b52d1caf04c8f6fff48ee96d2cc0570d
Accord99 <114819430+Accord99@users.noreply.github.com>
2023-08-18 09:58:42 +0300

    Создали миграцию для класса сити

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    slag = models.CharField(max_length=50,blank=True)
+
